d73edc4e64a6e83a2699f0a34c9c4471c778b768
Nathan Clevenger <nateclev@gmail.com>
2022-07-31 04:56:59 -0500

    Update schema.js

--- schema.js
+++ schema.js
@@ -1,9 +1,11 @@
-import { object, string, number, date } from 'yup'
+import { object, string, number, date, InferType } from 'yup'
 
-export const User = object({
+export const userSchema = object({
   name: string().required(),
   age: number().required().positive().integer(),
   email: string().email(),
   website: string().url().nullable(),
   createdOn: date().default(() => new Date()),
 })
+
+export type User = InferType<typeof userSchema>
